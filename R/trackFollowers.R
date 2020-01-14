1475b9f83e39c88a38ab07825e4d0991da78fbe6
friBo <friedricht.bohn@ufz.de>
2020-01-14 09:33:10 +0100

    first analysis of growth rate of followers

--- R/trackFollowers.R
+++ R/trackFollowers.R
@@ -22,12 +22,29 @@ Accounts <- c(# organisation
              , "wozukunft"
              , "adam_tooze"
              , "maike_wilhelm"
+             , "BohnFriedrich"
+             , "12062020olympia"
                     
 )
 
 
-readRDS("data/follower.rds")
+follower <- readRDS("data/follower.rds")
+#not nice should be better programmed:
+ if(ncol(follower)-1!=length(Accounts)){
+   add <- (ncol(follower)):length(Accounts)
+   follower <- cbind(follower, NA)
+   colnames(follower)[ncol(follower)] <- Accounts[length(Accounts)]
+ }
+
  followers <- c(NA,lookup_users(Accounts)$followers_count)
  follower <- rbind(follower,followers)
  follower[nrow(follower),1] <- date()
 saveRDS(follower, "data/follower.rds") 
+
+growthRate <- (follower[2:nrow(follower),2:ncol(follower)]/follower[1:(nrow(follower)-1),2:ncol(follower)]-1)*100
+
+plot(1,1,col="white", ylim=c(-1,15),xlim=c(1,nrow(growthRate)))
+for( i in 1:ncol(growthRate)){
+  lines(growthRate[,i])
+}
+     
